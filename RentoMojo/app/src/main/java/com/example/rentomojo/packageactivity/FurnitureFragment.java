a037632a0a0141b76750182808b4c75415ffaae6
sanjoy <paulsanjoy9875@gmail.com>
2021-02-04 20:47:20 +0530

    furniture_fragment

--- RentoMojo/app/src/main/java/com/example/rentomojo/packageactivity/FurnitureFragment.java
+++ RentoMojo/app/src/main/java/com/example/rentomojo/packageactivity/FurnitureFragment.java
@@ -2,16 +2,26 @@ package com.example.rentomojo.packageactivity;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.rentomojo.R;
+import com.example.rentomojo.recyclerview.CategoryItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class FurnitureFragment extends Fragment {
+
+    private RecyclerView recyclerView;
 
     public static FurnitureFragment newInstance(){
         FurnitureFragment furnitureFragment = new FurnitureFragment();
@@ -26,5 +36,43 @@ public class FurnitureFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_furniture, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView=view.findViewById(R.id.recyclerView);
+        buildRecyclerViewData();
+    }
+
+    private void buildRecyclerViewData() {
+
+        List<PackageItem> packageItemList_1 = new ArrayList<>();
+        for (int i = 0; i < 200; i++) {
+            if (i % 6 == 0) {
+                packageItemList_1.add(new PackageItem(R.drawable.sofa_table, "Sofa & Table", "Rs 20,000"));
+            }
+            if (i % 6 == 1) {
+                packageItemList_1.add(new PackageItem(R.drawable.home_decoration, "Room Decoration", "Rs 25,000"));
+            }
+            if (i % 6 == 2) {
+                packageItemList_1.add(new PackageItem(R.drawable.book_shelf, "Book Shelf", "Rs 14,400"));
+            }
+            if (i % 6 == 3) {
+                packageItemList_1.add(new PackageItem(R.drawable.shelf, "Shoe Shelf", "Rs 14,400"));
+            }
+            if (i % 6 == 4) {
+                packageItemList_1.add(new PackageItem(R.drawable.study_table, "Study Table Combo", "Rs 8,000"));
+            }
+            if (i % 6 == 5) {
+                packageItemList_1.add(new PackageItem(R.drawable.bed_package, "Best Package", "Rs 8,000"));
+            }
+        }
+
+        GridLayoutManager grid = new GridLayoutManager(getContext(),2);
+        recyclerView.setLayoutManager(grid);
+        FurnitureAdapter furnitureAdapter = new FurnitureAdapter(packageItemList_1);
+        recyclerView.setAdapter(furnitureAdapter);
+
     }
 }
